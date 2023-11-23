38068146dce957bd11a4461186394a7d26711755
mTrujillf <miguel.trujillf@gmail.com>
2023-11-23 13:56:08 -0600

    Menu bebidas en grid

--- cafeteria-app/src/App.js
+++ cafeteria-app/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import { BebidasProvider } from "./CatalogoBebidasContext";
+import { BebidaSeleccionadaProvider } from "./BebidaSeleccionadaContext";
+
 import Navbar from "./components/BarraNavegacion";
 import CatalogoBebidas from "./components/CatalogoBebidas";
 import MetodosPago from "./components/VentanaPago";
@@ -10,13 +12,15 @@ import PersonalizacionBebidas from "./components/PersonalizacionBebidas";
 function App() {
 	return (
 		<BebidasProvider>
-			<div className="App">
-				<Navbar />
-				<CatalogoBebidas />
-				{/* <MetodosPago />
-				<Retroalimentacion />
-				<PersonalizacionBebidas /> */}
-			</div>
+			<BebidaSeleccionadaProvider>
+				<div className="App">
+					<Navbar />
+					<CatalogoBebidas />
+					{/* <MetodosPago />
+					<Retroalimentacion />
+					<PersonalizacionBebidas /> */}
+				</div>
+			</BebidaSeleccionadaProvider>
 		</BebidasProvider>
 	);
 }
--- cafeteria-app/src/components/CatalogoBebidas.jsx
+++ cafeteria-app/src/components/CatalogoBebidas.jsx
@@ -1,40 +1,37 @@
 import React, { useContext } from "react";
-import "./CatalogoBebidas.css";
+import "./CatalogoBebidas.css"; // Asegúrate de actualizar este archivo con nuevos estilos
 import { ContextoBebidas } from "../CatalogoBebidasContext";
+import { ContextoBebidaSeleccionada } from "../BebidaSeleccionadaContext";
 
 const CatalogoBebidas = () => {
 	const { bebidas } = useContext(ContextoBebidas);
+	const { setBebidaSeleccionada } = useContext(ContextoBebidaSeleccionada);
+
+	const seleccionarBebida = (bebida) => {
+		setBebidaSeleccionada(bebida);
+		// Aquí puedes implementar la lógica de redirección si es necesario
+	};
 
 	return (
-		<div className="catalogo-bebidas">
-			<table>
-				<thead>
-					<tr>
-						<th>Imagen</th>
-						<th>Nombre</th>
-						<th>Tamaños Disponibles</th>
-						<th>Precio</th>
-						<th>Rating</th>
-					</tr>
-				</thead>
-				<tbody>
-					{bebidas.map((bebida) => (
-						<tr key={bebida.id}>
-							<td>
-								<img
-									src={bebida.imagen}
-									alt={bebida.nombre}
-									height="200px"
-								/>
-							</td>
-							<td>{bebida.nombre}</td>
-							<td>{bebida.tamanos.join(", ")}</td>
-							<td>${bebida.precio}</td>
-							<td>{"★".repeat(bebida.rating) + "☆".repeat(5 - bebida.rating)}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
+		<div className="grid-container">
+			{bebidas.map((bebida) => (
+				<div
+					key={bebida.id}
+					className="grid-item"
+					onClick={() => seleccionarBebida(bebida)}
+				>
+					<img
+						src={bebida.imagen}
+						alt={bebida.nombre}
+						className="imagen-bebida"
+					/>
+					<div className="info-bebida">
+						<h3>{bebida.nombre}</h3>
+						<p>{"★".repeat(bebida.rating) + "☆".repeat(5 - bebida.rating)}</p>
+						<p>${bebida.precio}</p>
+					</div>
+				</div>
+			))}
 		</div>
 	);
 };

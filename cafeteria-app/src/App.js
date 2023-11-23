4cadbad2301add5f752806112f5a4984e0d911e8
mTrujillf <miguel.trujillf@gmail.com>
2023-11-23 16:08:43 -0600

    inicio pantalla personalizacion

--- cafeteria-app/src/App.js
+++ cafeteria-app/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./App.css";
 import { BebidasProvider } from "./CatalogoBebidasContext";
 import { BebidaSeleccionadaProvider } from "./BebidaSeleccionadaContext";
+import { OrdenProvider } from "./OrdenContexto";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/BarraNavegacion";
 import CatalogoBebidas from "./components/CatalogoBebidas";
 import MetodosPago from "./components/VentanaPago";
@@ -14,25 +14,27 @@ function App() {
 	return (
 		<BebidasProvider>
 			<BebidaSeleccionadaProvider>
-				<Router>
-					<Routes>
-						<Route
-							path="/"
-							element={<CatalogoBebidas />}
-						/>
-						<Route
-							path="/personalizacion"
-							element={<PersonalizacionBebidas />}
-						/>
-					</Routes>
-				</Router>
-				{/* <div className="App">
+				<OrdenProvider>
+					<Router>
+						<Routes>
+							<Route
+								path="/"
+								element={<CatalogoBebidas />}
+							/>
+							<Route
+								path="/personalizacion"
+								element={<PersonalizacionBebidas />}
+							/>
+						</Routes>
+					</Router>
+					{/* <div className="App">
 					{/* <Navbar />
 					<CatalogoBebidas /> */}
-				{/* <MetodosPago />
+					{/* <MetodosPago />
 					<Retroalimentacion />
 					<PersonalizacionBebidas /> */}
-				{/* </div>*/}
+					{/* </div>*/}
+				</OrdenProvider>
 			</BebidaSeleccionadaProvider>
 		</BebidasProvider>
 	);
--- cafeteria-app/src/components/PersonalizacionBebidas.jsx
+++ cafeteria-app/src/components/PersonalizacionBebidas.jsx
@@ -1,8 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ContextoBebidaSeleccionada } from "../BebidaSeleccionadaContext";
+import { OrdenProvider } from "../OrdenContexto";
+import { ContextoOrden } from "../OrdenContexto";
 
 const PersonalizacionBebidas = () => {
 	const { bebidaSeleccionada } = useContext(ContextoBebidaSeleccionada);
+	const { orden, addToOrder } = useContext(ContextoOrden);
+	const [opcionesPersonalizacion, setOpcionesPersonalizacion] = useState({
+		tipoLeche: "entera",
+		edulcorante: "azúcar",
+	});
+
+	const manejarCambioLeche = (evento) => {
+		setOpcionesPersonalizacion({ ...opcionesPersonalizacion, tipoLeche: evento.target.value });
+	};
+
+	const manejarCambioEdulcorante = (evento) => {
+		setOpcionesPersonalizacion({ ...opcionesPersonalizacion, edulcorante: evento.target.value });
+	};
+
+	const manejarAgregarOrden = () => {
+		const bebidaConPersonalizacion = {
+			...bebidaSeleccionada,
+			personalizacion: opcionesPersonalizacion, // Asegúrate de que la estructura coincida
+		};
+		addToOrder(bebidaConPersonalizacion);
+	};
 
 	if (!bebidaSeleccionada) {
 		return <div>Seleccione una bebida para personalizar.</div>;
@@ -10,8 +33,44 @@ const PersonalizacionBebidas = () => {
 
 	return (
 		<div>
-			<h2>Personaliza tu bebida: {bebidaSeleccionada.nombre}</h2>
-			{/* Más lógica de personalización aquí */}
+			<h2>Personaliza tu bebida: {bebidaSeleccionada?.nombre}</h2>
+			{/* Ejemplo de selección de tipo de leche */}
+			<select
+				value={opcionesPersonalizacion.tipoLeche}
+				onChange={manejarCambioLeche}
+			>
+				<option value="entera">Leche entera</option>
+				<option value="deslactosada">Leche deslactosada</option>
+				<option value="vegetal">Leche vegetal</option>
+				{/* otras opciones */}
+			</select>
+
+			{/* Ejemplo de selección de edulcorantes */}
+			<select
+				value={opcionesPersonalizacion.edulcorante}
+				onChange={manejarCambioEdulcorante}
+			>
+				<option value="azúcar">Azúcar</option>
+				<option value="stevia">Stevia</option>
+				{/* otras opciones */}
+			</select>
+
+			<button onClick={manejarAgregarOrden}>Añadir a la orden</button>
+
+			<div>
+				<h3>Orden actual:</h3>
+				{orden.length > 0 ? (
+					<ul>
+						{orden.map((item, index) => (
+							<li key={index}>
+								{item.nombre} - {item.personalizacion.tipoLeche}, {item.personalizacion.edulcorante}
+							</li>
+						))}
+					</ul>
+				) : (
+					<p>No hay bebidas en tu orden.</p>
+				)}
+			</div>
 		</div>
 	);
 };

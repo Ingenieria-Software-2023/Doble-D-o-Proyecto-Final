655f91792610948ee3f3f32a7ac88ece26ea370f
Santiago <chocs.whatever@gmail.com>
2023-11-24 00:34:02 -0600

    Cambios a la informacion nutrional

--- src/components/PersonalizacionBebidas.jsx
+++ src/components/PersonalizacionBebidas.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ContextoBebidaSeleccionada } from "../BebidaSeleccionadaContext";
 import { ContextoOrden } from "../OrdenContexto";
 import "./PersonalizacionBebidas.css";
